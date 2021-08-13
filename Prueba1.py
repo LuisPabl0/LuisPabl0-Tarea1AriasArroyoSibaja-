87dca34f785dcf948aee142912b6db411c829427
LuisPabl0 <luispasp97@gmail.com>
2021-08-13 00:36:46 -0600

    Se agrega el codigo de funciones y el codigo de testing

---
+++ Prueba1.py
@@ -0,0 +1,42 @@
+class Prueba1:
+    # Se define la funcion multiple_op
+    def multiple_op(numero):
+        # Se inicializa el ciclo
+        while True:
+            try:
+                # La entrada debe ser un numero
+                int(numero)
+                # Se realizan las operaciones solicitas
+                op1 = numero * numero
+                op2 = 2 ** numero
+                # Operacion de factorial
+                op3 = 1
+                for i in range(1, numero + 1):
+                    op3 = op3 * i
+                resultado = [op1, op2, op3]
+                # Retrona la lista del resultado
+                return (resultado)
+            # Cualquier error se recibe en el Exception
+            except Exception:
+                return "Debe escribir un numero entero y positivo"
+
+    def verify_array_op(entrada):
+        # Se inicializa el ciclo
+        while True:
+            try:
+                # Determinar la cantidad de valores de la lista
+                rango = len(entrada)
+                resultado = []
+                salida = []
+                # Se define i como contador
+                i = 0
+                # Se realizan las operaciones solicitas
+                for i in range(rango):
+                    numero = int(entrada[i])
+                    salida = Prueba1.multiple_op(numero)
+                    resultado.append(salida)
+                # Retorna la lista del resultado
+                return (resultado)
+            # Cualquier error se recibe en el Exception
+            except Exception:
+                return "Debe escribir un numero entero y positivo"
---
+++ test_prueba.py
@@ -0,0 +1,57 @@
+# Se importan las librerias requeridas
+import random
+from Prueba1 import Prueba1
+
+
+# Caso de exito del metodo multiple_op
+def test_prueba():
+    # Se crean valores aleatorios de entrada
+    num = random.randint(1, 100)
+    # Se revisa que la entrada es un numero
+    assert isinstance(num, int)
+    # Se revisa que la salida sea una lista
+    assert isinstance(Prueba1.multiple_op(num), list)
+
+
+# Caso de error del metodo multiple_op
+def test_prueba2():
+    # Se define una entrada erronea
+    num = "s"
+    # Se revisa que la entrada es un numero
+    assert isinstance(num, int)
+    # Se revisa que la salida sea una lista
+    assert isinstance(Prueba1.multiple_op(num), list)
+
+
+# Caso de exito del metodo verify_array_op
+def test_prueba3():
+    # Se define i como contador y se inicializan las variables
+    i = 0
+    entrada = []
+    # Se crea el parametro de entrada
+    for i in range(2):
+        valor = random.randint(1, 100)
+        # Verificar que son numeros
+        assert isinstance(valor, int)
+        entrada.append(valor)
+    # Verificar que es una lista
+    assert isinstance(entrada, list)
+    # Verifica el resultado
+    assert isinstance(Prueba1.verify_array_op(entrada), list)
+
+
+# Caso de error del metodo verify_array_op
+def test_prueba4():
+    # Se define i como contador y se inicializan las variables
+    entrada = []
+    i = 0
+    # Se crea el parametro de entrada erroneo
+    for i in range(2):
+        valor = "a"
+        # Verificar que son numeros
+        assert isinstance(valor, int)
+        entrada[i] = valor
+    # Verificar que es una lista
+    assert isinstance(entrada, list)
+    # Verifica el resultado
+    assert isinstance(Prueba1.verify_array_op(entrada), list)
